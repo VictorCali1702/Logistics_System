c7681a5be1ae1d6850033afe80108aac2c50a03e
VictorCali1702 <wiktor.naczk199@gmail.com>
2025-12-14 20:00:03 +0100

    Hahing

---
+++ logistic-system/utils/security.py
@@ -0,0 +1,9 @@
+# Security of date our customers, by hashing passwords.
+
+import hashlib
+
+def hash_password(password: str) -> str:
+	return hashlib.sha256(password.encode()).hexdigest()
+
+def verify_password(password: str, hashed: str) -> bool:
+	return hash_password(password) == hashed
